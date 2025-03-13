5bc5a9d58be520274a4c35def071cedfb0ee72d0
Frivox1 <mertens.valery@gmail.com>
2025-03-13 18:05:48 +0100

    v1 almost finished

--- scripts.js
+++ scripts.js
@@ -4,17 +4,44 @@ const faqToggles = document.querySelectorAll('.faq-toggle');
 // Ajoute un événement de clic à chaque bouton
 faqToggles.forEach(toggle => {
     toggle.addEventListener('click', function() {
-        const content = this.nextElementSibling; // Le contenu qui suit le bouton
+        const content = this.nextElementSibling;
         const isExpanded = this.getAttribute('aria-expanded') === 'true';
 
-        // Change l'attribut aria-expanded pour indiquer si la section est ouverte ou fermée
         this.setAttribute('aria-expanded', !isExpanded);
 
-        // Anime l'ouverture et la fermeture du contenu
         if (isExpanded) {
-            content.style.maxHeight = 0; // Ferme le contenu
+            content.style.maxHeight = 0;
         } else {
-            content.style.maxHeight = content.scrollHeight + 'px'; // Ouvre le contenu
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const downloadBtn = document.querySelector(".btn[download]");
+    const modal = document.getElementById("download-modal");
+    const closeModal = document.querySelector(".close");
+
+    if (downloadBtn) {
+        downloadBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            modal.classList.add("show");
+            
+            setTimeout(() => {
+                window.location.href = downloadBtn.href;
+            }, 1000);
+        });
+    }
+
+    if (closeModal) {
+        closeModal.addEventListener("click", function () {
+            modal.classList.remove("show");
+        });
+    }
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.classList.remove("show");
         }
     });
 });
